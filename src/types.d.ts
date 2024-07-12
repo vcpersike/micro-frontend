c61b15ec055aaed35ec6c0ea4679497989666936
vcpersike <vcpersike@gmail.com>
2024-07-11 23:28:33 -0300

    ajustes

---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+declare module 'microFront01/Button' {
+    const Button: React.ComponentType;
+    export default Button;
+  }
+
+  declare module 'microFront01/ReactLogo' {
+    const ReactLogo: React.ComponentType;
+    export default ReactLogo;
+  }
