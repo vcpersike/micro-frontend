1415f159474ba4ab8e55fbdf1b014a8131ec471b
vcpersike <vcpersike@gmail.com>
2024-07-11 23:28:59 -0300

    Mais um ajuste de teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css'
-import React from "react";
+import Button from "microFront01/Button";
 
-const Button = React.lazy(() => import('microFront01/Button'));
-const ReactLogo = React.lazy(() => import('microFront01/ReactLogo'));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Button />,
-    children: [ {
-      index: true,
-      element: (
-        <div>
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <Button />
-            <ReactLogo />
-          </React.Suspense>
-        </div>
-      ),
-    },],
+    children: [],
   },
 ]);
 
